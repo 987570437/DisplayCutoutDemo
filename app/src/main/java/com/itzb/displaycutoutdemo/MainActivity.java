8466b05cc2b5ec71d460788f54d2f77eb5a03fb3
987570437 <987570437@qq.com>
2019-10-11 00:06:41 +0800

    提交代码

---
+++ app/src/main/java/com/itzb/displaycutoutdemo/MainActivity.java
@@ -0,0 +1,84 @@
+package com.itzb.displaycutoutdemo;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.DisplayCutout;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowInsets;
+import android.view.WindowManager;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //1.设置全屏
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        Window window = getWindow();
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //2.判断手机是否刘海
+        boolean isDisplayCutout = isDisplayCutout(window);
+
+        //3.设置是否让内容区域延伸进刘海及是否设置成沉浸式
+        if (isDisplayCutout) {//让内容区域延伸进刘海
+
+            WindowManager.LayoutParams params = window.getAttributes();
+            /**
+             *  * @see #LAYOUT_IN_DISPLAY_CUTOUT_MODE_DEFAULT 全屏模式，内容下移，非全屏不受影响
+             *  * @see #LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES 允许内容去延伸进刘海区
+             *  * @see #LAYOUT_IN_DISPLAY_CUTOUT_MODE_NEVER 不允许内容延伸进刘海区
+             */
+            params.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+            window.setAttributes(params);
+
+            //设置成沉浸式
+            int flags = View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+            int visibility = window.getDecorView().getSystemUiVisibility();
+            visibility |= flags; //追加沉浸式设置
+            window.getDecorView().setSystemUiVisibility(visibility);
+        }
+
+        setContentView(R.layout.activity_main);
+
+        View view = findViewById(R.id.view);
+        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) view.getLayoutParams();
+        params.topMargin = heightForDisplayCutout();
+        view.setLayoutParams(params);
+
+    }
+
+    private boolean isDisplayCutout(Window window) {
+        DisplayCutout displayCutout;
+        View rootView = window.getDecorView();
+        WindowInsets insets = rootView.getRootWindowInsets();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P && insets != null) {
+            displayCutout = insets.getDisplayCutout();
+            if (displayCutout != null) {
+                if (displayCutout.getBoundingRects() != null && displayCutout.getBoundingRects().size() > 0 && displayCutout.getSafeInsetTop() > 0) {
+                    return true;
+                }
+            }
+        }
+        return false; //模拟器可能获取失败，模拟器演示时将此处改为true看效果
+    }
+
+
+    //通常情况下，刘海的高就是状态栏的高
+    public int heightForDisplayCutout() {
+        int resID = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resID > 0) {
+            return getResources().getDimensionPixelSize(resID);
+        }
+        return 96;
+    }
+
+}
